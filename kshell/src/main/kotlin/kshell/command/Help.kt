8600f623d77902aab89bedc85fae84e838f099c2
khud <vitaly.khudobakhshov@gmail.com>
2017-06-09 18:07:09 +0300

    Minor code style improvements

--- kshell/src/main/kotlin/kshell/command/Help.kt
+++ kshell/src/main/kotlin/kshell/command/Help.kt
@@ -4,18 +4,20 @@ import kshell.Command
 
 open class Help : Command("help", "h", "print this summary or command-specific help", params = "[command]") {
     override fun execute(line: String) {
-        val repl = this.repl!!
         val args = line.split(' ')
-        if (args.size == 1) {
-            val help = repl.commands.map(Command::toString).joinToString(separator = "\n")
-            repl.reader.println(help)
-        } else {
-            val command = args[1]
-            val res = repl.commands.filter { it.match(":$command") }
-            if (res.isEmpty()) {
-                repl.reader.println("$command: no such command.  Type :help for help.")
+
+        repl!!.apply {
+            if (args.size == 1) {
+                val help = commands.map(Command::toString).joinToString(separator = "\n")
+                reader.println(help)
             } else {
-                repl.reader.println(res[0].signature() + res[0].help())
+                val command = args[1]
+                val res = commands.filter { it.match(":$command") }
+                if (res.isEmpty()) {
+                    reader.println("$command: no such command.  Type :help for help.")
+                } else {
+                    reader.println(res[0].signature() + res[0].help())
+                }
             }
         }
     }
