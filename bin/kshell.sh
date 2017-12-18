427b67681abdcd3aff733df76dccf9d34ce6a34c
khud <vitaly.khudobakhshov@gmail.com>
2017-12-18 17:04:40 +0300

    kshell run script

--- bin/kshell.sh
+++ bin/kshell.sh
@@ -9,5 +9,7 @@ KOTLIN_REFLECT=$MAVEN_REPO_BASE/kotlin-reflect/$KOTLIN_VERSION/kotlin-reflect-$K
 KOTLIN_IDE_COMMON=$MAVEN_REPO_BASE/kotlin-ide-common/SNAPSHOT/kotlin-ide-common-SNAPSHOT.jar
 FUSE_JANSI=$HOME/.m2/repository/org/fusesource/jansi/jansi/1.15/jansi-1.15.jar
 
+# FIXME: check paths before run
+
 JARS=$KOTLIN_COMPILER:$KOTLIN_STDLIB:$KOTLIN_REFLECT:$KOTLIN_SCRIPT_RUNTIME:$KOTLIN_IDE_COMMON
 java -classpath $JARS:kshell/target/kshell-SNAPSHOT.jar kshell.KotlinShell $@
