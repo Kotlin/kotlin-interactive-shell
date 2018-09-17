48d73283b9e53be2ea87aec40a44298b6aa51632
khud <vitaly.khudobakhshov@gmail.com>
2018-09-17 13:33:48 +0300

    Update version to 0.2.4

--- kshell/src/main/kotlin/com/github/khud/sparklin/kshell/KShell.kt
+++ kshell/src/main/kotlin/com/github/khud/sparklin/kshell/KShell.kt
@@ -265,7 +265,7 @@ open class KShell(val disposable: Disposable,
     }
 
     companion object {
-        const val VERSION: String = "0.2.3"
+        const val VERSION: String = "0.2.4"
     }
 }
 
