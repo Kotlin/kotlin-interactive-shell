1a7c19eee50680cbfee713d7b72a5ee7876e3b83
khud <vitaly.khudobakhshov@gmail.com>
2018-09-26 11:23:14 +0300

    Fix https://github.com/khud/sparklin/issues/31

--- kshell/src/main/kotlin/com/github/khud/sparklin/kshell/KShell.kt
+++ kshell/src/main/kotlin/com/github/khud/sparklin/kshell/KShell.kt
@@ -126,6 +126,7 @@ open class KShell(val disposable: Disposable,
         reader.setVariable(LineReader.HISTORY_FILE, configuration.get(LineReader.HISTORY_FILE,
                 System.getProperty("user.home") + File.separator + ".kshell_history"))
         reader.setVariable(LineReader.SECONDARY_PROMPT_PATTERN, "")
+        reader.option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
 
         compiler = ReplCompiler(disposable, compilerConfiguration, messageCollector)
         evaluator = ReplEvaluator(classpath, baseClassloader)
