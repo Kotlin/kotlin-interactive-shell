51bcdc409811c9734867a17b9102a1dffa5bed20
khud <vitaly.khudobakhshov@gmail.com>
2021-04-13 12:52:44 +0300

    Additions to #68: realpath workaround for non GNU systems

--- bin/ki.sh
+++ bin/ki.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+command -v realpath &> /dev/null || realpath() {
+  SCRIPT_PATH=$( [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}" )
+  if [[ -L $SCRIPT_PATH ]];
+  then
+    readlink $SCRIPT_PATH
+  else
+    echo $SCRIPT_PATH
+  fi
+}
+
 SCRIPT_DIR="$( dirname $(realpath "$0") )"
 KI_SHELL=$SCRIPT_DIR/../lib/ki-shell.jar
 
