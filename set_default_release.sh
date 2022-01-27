ee5e2a2ff407800f203efe5d611e6c40fc7b40c0
Pasha Finkelshteyn <asm0dey@jetbrains.com>
2022-01-27 13:00:17 +0300

    chore: Change script to setup default version

--- set_default_release.sh
+++ set_default_release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-VERSION="$0"
+VERSION="$1"
 
 xh PUT https://vendors.sdkman.io/default Consumer-Key:"$JRELEASER_SDKMAN_CONSUMER_KEY" Consumer-Token:"$JRELEASER_SDKMAN_CONSUMER_TOKEN" candidate=ki version="$VERSION"
 
